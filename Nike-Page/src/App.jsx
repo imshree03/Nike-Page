561dd2bb0be2797cfcf8fcd656245080b80c0bab
imshree03 <shreebhosale24hrs@gmail.com>
2023-10-24 16:45:34 +0530

    Nike Home Page Project1 Created

---
+++ Nike-Page/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/HeroSection";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ Nike-Page/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="./images/brand_logo.png" alt="" />
+      </div>
+
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
